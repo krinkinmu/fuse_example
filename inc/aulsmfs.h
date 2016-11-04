f0d043ef672dd2ced7eb0d92545dfdf9a4bf77e4
Mike Krinkin <krinkin.m.u@gmail.com>
2016-11-04 22:10:37 +0300

    add missed header file

---
+++ inc/aulsmfs.h
@@ -0,0 +1,45 @@
+#ifndef __AULSMFS_H__
+#define __AULSMFS_H__
+
+#include <stdint.h>
+
+
+#define AULSMFS_MAGIC	0x0A0153F5
+#define AULSMFS_MAJOR	0
+#define AULSMFS_MINOR	1
+#define AULSMFS_VERSION	(((uint64_t)AULSMFS_MAJOR << 32) | (AULSMFS_MINOR))
+
+
+/* We are using little endian since it's native byte order
+ * for the majority of existing architectures. */
+typedef uint8_t		le8_t;
+typedef uint16_t	le16_t;
+typedef uint32_t	le32_t;
+typedef uint64_t	le64_t;
+
+
+/* All offsets, sizes and so on are always given in pages unless
+ * explicitly stated otherwise. */
+struct aulsmfs_ptr {
+	le64_t offs;
+	le64_t size;
+	le64_t csum;
+} __attribute__((packed));
+
+struct aulsmfs_super {
+	le64_t magic;
+	le64_t version;
+	le64_t page_size;
+	le64_t pages;
+
+	/* Points at the "root" of the filesystem */
+	struct aulsmfs_ptr root;
+
+	le64_t csum;
+} __attribute__((packed));
+
+struct aulsmfs_root {
+	le64_t dummy;
+} __attribute__((packed));
+
+#endif /*__AULSMFS_H__*/
